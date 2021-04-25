a6139c2978468f6c19278c95665f640f0da7b7e3
luizmvb <luizmig52@gmail.com>
2021-04-25 05:02:41 -0300

    adicionando funcionalidades de exportação de arquivos de sequências fasta e taxonomia txt

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, send_file
 
 app = Flask(__name__)
+uploads_path = 'app/uploads/'
+table = []
 
 @app.route('/')
 def index():
@@ -10,6 +12,23 @@ def index():
 def main():
     return render_template('main.html')
 
-@app.route('/export')
-def export():
-    return "export"
+@app.route('/addItem/<id>/<seq>/<tax>')
+def addItem(id, seq, tax):
+    table.append({'id': id, 'seq': seq, 'tax': tax})
+    return {}
+
+@app.route('/export_seqs')
+def export_seqs():
+    f_seq = open(uploads_path + 'seqs.fasta', 'w')
+    for item in table:
+        f_seq.write('>' + str(item['id']) + '\n' + item['seq'] + '\n')
+    f_seq.close()
+    return send_file('uploads/seqs.fasta', as_attachment=True)
+    
+@app.route('/export_tax')
+def export_tax():
+    f_tax = open(uploads_path + 'taxonomy.txt', 'w')
+    for item in table:
+        f_tax.write(str(item['id']) + '\t' + item['tax'] + '\n')
+    f_tax.close()
+    return send_file('uploads/taxonomy.txt', as_attachment=True)
--- app/static/js/add.js
+++ app/static/js/add.js
@@ -1,10 +1,11 @@
-var table = [];
-
+var id = 0;
 function addItem(){
+    id++;
     let seq = document.getElementById('seq').value;
     let tax = document.getElementById('tax').value;
     if(seq && tax) {
-        table.push({seq : seq, tax : tax});
-        document.getElementById('items').insertAdjacentHTML("beforeend", "<tr><td>" + seq + "</td><td>" + tax + "</td></tr>");
+        document.getElementById('items').insertAdjacentHTML('beforeend', '<tr><td>' + seq + '</td><td>' + tax + '</td></tr>');
+        fetch('/addItem/' + id + '/' + seq + '/' + tax);
     }
+
 }
