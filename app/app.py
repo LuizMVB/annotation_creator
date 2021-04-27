a5f320ebb70be5b3cd686659681ad55714d61499
luizmvb <luizmig52@gmail.com>
2021-04-27 11:00:43 -0300

    adicionando tratamento de texto para cada linha de sequência e taxonomia adicionadas, modificando lógica de criação e substituição de arquivo

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, redirect, url_for, render_template, request, send_file
+import os, re
 
 app = Flask(__name__)
 uploads_path = 'app/uploads/'
-table = []
+if os.path.exists("app/uploads/seqs.fasta"):
+    os.remove('app/uploads/seqs.fasta')
+if os.path.exists("app/uploads/taxonomy.txt"):
+    os.remove('app/uploads/taxonomy.txt')
 
 @app.route('/')
 def index():
@@ -14,21 +18,26 @@ def main():
 
 @app.route('/addItem/<id>/<seq>/<tax>')
 def addItem(id, seq, tax):
-    table.append({'id': id, 'seq': seq, 'tax': tax})
+    addSeqLine(id, seq)
+    addTaxLine(id, tax)
     return {}
 
 @app.route('/export_seqs')
 def export_seqs():
-    f_seq = open(uploads_path + 'seqs.fasta', 'w')
-    for item in table:
-        f_seq.write('>' + str(item['id']) + '\n' + item['seq'] + '\n')
-    f_seq.close()
     return send_file('uploads/seqs.fasta', as_attachment=True)
     
 @app.route('/export_tax')
 def export_tax():
-    f_tax = open(uploads_path + 'taxonomy.txt', 'w')
-    for item in table:
-        f_tax.write(str(item['id']) + '\t' + item['tax'] + '\n')
-    f_tax.close()
     return send_file('uploads/taxonomy.txt', as_attachment=True)
+
+def addSeqLine(id, seq):
+    seq = seq.replace('\n', '')
+    f_seq = open(uploads_path + 'seqs.fasta', 'a')
+    f_seq.write('>' + id + '\n' + seq + '\n')
+    f_seq.close()
+
+def addTaxLine(id, tax):
+    tax = tax.replace(';', '; ').replace('  ', ' ').replace('.', '')
+    f_tax = open(uploads_path + 'taxonomy.txt', 'a')
+    f_tax.write(id + '\t' + tax + '\n')
+    f_tax.close()
